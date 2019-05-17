8e4c69d047d31fca8225b83df72c393e3d4b83d5
elod <chr.elod96@gmail.com>
2019-05-17 12:06:15 +0200

    More tests added.

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -46,5 +46,62 @@ public class GT4500Test {
     verify(MockPrimaryTs, times(1)).fire(1);
     verify(MockSecondaryTs, times(1)).fire(1);
   }
+  
+    @Test
+  public void fireTorpedo_All_PRIMARY_FAIL(){
+    // Arrange
+	when(MockPrimaryTs.isEmpty()).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.ALL);
+  }
+  
+  @Test
+  public void fireTorpedo_All_SECONDARY_FAIL(){
+    // Arrange
+	when(MockSecondaryTs.isEmpty()).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.ALL);
+  }
+  
+    @Test
+  public void fireTorpedo_SINGLE_FAIL(){
+    // Arrange
+	when(MockPrimaryTs.isEmpty()).thenReturn(false);
+	when(MockSecondaryTs.isEmpty()).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+  }
+  
+   @Test
+  public void fireTorpedo_SINGLE_SUCCESS1(){
+    // Arrange
+	when(MockPrimaryTs.isEmpty()).thenReturn(true);
+    when(MockSecondaryTs.fire(1)).thenReturn(true);
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+	
+	verify(MockSecondaryTs, times(1)).fire(1);
+  }
+  
+   @Test
+  public void fireTorpedo_SINGLE_SUCCESS2(){
+    // Arrange
+	when(MockSecondaryTs.isEmpty()).thenReturn(true);
+    when(MockPrimaryTs.fire(1)).thenReturn(true);
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+	
+	verify(MockPrimaryTs, times(1)).fire(1);
+  }
+  
+   @Test
+  public void fireTorpedo_Single_Fail(){
+    // Arrange
+	when(MockPrimaryTs.fire(0)).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
 
+    // Assert
+    verify(MockPrimaryTs, times(1)).fire(1);
+  }
 }
